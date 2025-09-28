a64650c7a79c66c2d2e26bca5cc5dc0b2d44db3f
ahb <andreas.bihlmaier@gmail.com>
2025-09-28 11:02:43 -0700

    ament_clang_format

--- src/sdf_to_urdf.cpp
+++ src/sdf_to_urdf.cpp
@@ -1,35 +1,36 @@
+#include <tinyxml2.h>
+#include <urdf/model.h>
+#include <urdf_parser/urdf_parser.h>
+
 #include <fstream>
 #include <iostream>
 #include <sstream>
 
-#include <urdf/model.h>
-#include <urdf_parser/urdf_parser.h>
-#include <tinyxml2.h>
-
-int main(int argc, char** argv) {
+int main(int argc, char ** argv)
+{
   if (argc < 3) {
     std::cerr << "Usage: sdf_to_urdf <input_sdf_file> <output_urdf_file>" << std::endl;
     return 1;
   }
   std::string sdf_file = argv[1];
-	std::ifstream file(sdf_file);
-	if (!file) {
-		std::cerr << "Failed to open SDF file: " << sdf_file << std::endl;
-		return 1;
-	}
-	std::stringstream buffer;
-	buffer << file.rdbuf();
-	std::string sdf_str = buffer.str();
+  std::ifstream file(sdf_file);
+  if (!file) {
+    std::cerr << "Failed to open SDF file: " << sdf_file << std::endl;
+    return 1;
+  }
+  std::stringstream buffer;
+  buffer << file.rdbuf();
+  std::string sdf_str = buffer.str();
 
-	urdf::Model model;
+  urdf::Model model;
   // This works due to sdformat_urdf
-	if (!model.initString(sdf_str)) {
-		std::cerr << "Failed to parse SDF file: " << sdf_file << std::endl;
-		return 1;
-	}
-	std::cout << "Successfully loaded SDF model: " << model.getName() << std::endl;
+  if (!model.initString(sdf_str)) {
+    std::cerr << "Failed to parse SDF file: " << sdf_file << std::endl;
+    return 1;
+  }
+  std::cout << "Successfully loaded SDF model: " << model.getName() << std::endl;
 
-  tinyxml2::XMLDocument* urdf_xml_doc = urdf::exportURDF(model);
+  tinyxml2::XMLDocument * urdf_xml_doc = urdf::exportURDF(model);
 
   std::string output_file = argv[2];
   tinyxml2::XMLError save_result = urdf_xml_doc->SaveFile(output_file.c_str());
